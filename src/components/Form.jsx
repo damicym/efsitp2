c168648d6e6f1cebf7dd6067900fbdc12320558d
JONATAN NAIFELD <49549062@ort>
2026-04-21 09:46:27 -0300

    required

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,15 +14,15 @@ function Form({ setCitas }) {
             <h2>Crear mi Cita</h2>
                 <form onSubmit={(e) => handleSubmit(e)}>
                     <label>Nombre Mascota</label>
-                    <input placeholder="Nombre Mascota" type="text" className="u-full-width" name="nomMascota"></input>
+                    <input placeholder="Nombre Mascota" type="text" className="u-full-width" name="nomMascota" required></input>
                     <label>Nombre Dueño</label>
-                    <input placeholder="Nombre dueño de la mascota" type="text" className="u-full-width" name="nomOwner"></input>
+                    <input placeholder="Nombre dueño de la mascota" type="text" className="u-full-width" name="nomOwner" required></input>
                     <label>Fecha</label>
-                    <input type="date" className="u-full-width" name="fecha"></input>
+                    <input type="date" className="u-full-width" name="fecha" required></input>
                     <label>Hora</label>
-                    <input type="time" className="u-full-width" name="hora"></input>
+                    <input type="time" className="u-full-width" name="hora" required></input>
                     <label>Síntomas</label>
-                    <textarea className="u-full-width" name="sintomas"></textarea>
+                    <textarea className="u-full-width" name="sintomas" required></textarea>
                     <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
             </form>
         </>
